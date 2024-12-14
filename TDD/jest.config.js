137265cdb2981691fafa20952fc4cddbc0ac1508
Luís Antônio dos Santos Silva <luisopentec@gmail.com>
2024-12-14 17:40:18 -0300

    Delete TDD directory

--- TDD/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  
-};
